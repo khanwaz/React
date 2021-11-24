22447c1c471174570d223c4a0f52346b54e67502
Akram khan <khanwaz@gmail.com>
2021-11-25 00:09:36 +0530

    updated till submit and event

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(datas) {
+  const [title, setTitle] = useState(datas.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!!!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={datas.date} />
       <div className="expense-item__description">
-        <h2>{datas.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${datas.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
