6f1334af3466371fd09c5618cc7ff185d0030f20
Akram khan <khanwaz@gmail.com>
2021-12-01 19:35:03 +0530

    updated code

--- my-first-react-project/src/App.js
+++ my-first-react-project/src/App.js
@@ -3,7 +3,7 @@ import FirstComponent from "./components/FirstComponent";
 import Button from "react-bootstrap/Button";
 function App() {
   return (
-    <div className="App">
+    <div>
       <h2>This is the first app done by aleena's dad.</h2>
       <FirstComponent></FirstComponent>
       <Button variant="dark">Click_ME</Button>
--- my-first-react-project/src/components/FirstComponent.js
+++ my-first-react-project/src/components/FirstComponent.js
@@ -1,9 +1,11 @@
+import NewDetailsForm from "./NewDetails/NewDetailsForm";
 import Button from "react-bootstrap/Button";
 function FirstComponent() {
   return (
     <div>
       <Button variant="dark">Click me</Button>
       <p> This is the First Component </p>
+      <NewDetailsForm></NewDetailsForm>
     </div>
   );
 }
