7defd9c7d531f1994c16a8a3d98c311a8f54be57
Akram khan <khanwaz@gmail.com>
2022-02-11 20:34:18 +0530

    new updates

--- my-first-react-project/src/components/Login/Loginpage.js
+++ my-first-react-project/src/components/Login/Loginpage.js
@@ -1,7 +1,37 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/esm/Col";
+import "./LoginPage.css";
+
 function LoginPage() {
   return (
-    <div>
-      <h3>This is the Login Page</h3>
+    <div >
+      <Container className="container" >
+        <Row >
+          <Col>
+            <Form className="form">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
