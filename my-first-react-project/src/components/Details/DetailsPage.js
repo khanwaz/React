3eb85c88ff2178ea06537c1875f6d2ba4efb8249
Akram khan <khanwaz@gmail.com>
2022-02-12 11:52:12 +0530

    All the adjustments

--- my-first-react-project/src/components/Details/DetailsPage.js
+++ my-first-react-project/src/components/Details/DetailsPage.js
@@ -12,18 +12,18 @@ function DetailsPage() {
     <div>
       <Container className="container">
         <Form className="form">
-          <Row className="mb-3">
+          <Row className="mb-4">
             <Col sm={2}>
               <Form.Group as={Col}>
                 <Form.Label>Name</Form.Label>
               </Form.Group>
             </Col>
-
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridName">
                 <Form.Control placeholder="Enter your name" />
               </Form.Group>
             </Col>
+
             <Col sm={2}>
               <Form.Group as={Col}>
                 <Form.Label>DOB</Form.Label>
@@ -31,17 +31,20 @@ function DetailsPage() {
             </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridDOB">
-                {/* <Form.Label>DOB</Form.Label> */}
                 <Form.Control placeholder="Enter DOB" />
               </Form.Group>
             </Col>
           </Row>
 
-          <Row className="mb-3">
+          <Row className="mb-4">
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Qualification</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Dropdown>
                 <Form.Group as={Col} controlId="formGridQualification">
-                  <Form.Label>Qualification</Form.Label>
                   <Dropdown>
                     <Dropdown.Toggle>Select the Qualification</Dropdown.Toggle>
                   </Dropdown>
@@ -49,59 +52,87 @@ function DetailsPage() {
               </Dropdown>
             </Col>
 
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Occupation</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridOccupation">
-                <Form.Label>Occupation</Form.Label>
                 <Form.Control type="text" placeholder="Enter Your Occupation" />
               </Form.Group>
             </Col>
           </Row>
 
-          <Row className="mb-3">
+          <Row className="mb-4">
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Email</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
               </Form.Group>
             </Col>
 
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Contact Number</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridContactNmber">
-                <Form.Label>Contact Number</Form.Label>
                 <Form.Control type="number" placeholder="Contact Number" />
               </Form.Group>
             </Col>
           </Row>
 
-          <Row className="mb-3">
+          <Row className="mb-4">
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Address</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridAddress1">
-                <Form.Label>Address</Form.Label>
                 <Form.Control placeholder="1234 Main St" />
               </Form.Group>
             </Col>
 
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Address 2</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridAddress2">
-                <Form.Label>Address 2</Form.Label>
                 <Form.Control placeholder="Apartment, studio, or floor" />
               </Form.Group>
             </Col>
           </Row>
 
           <Row className="mb-3">
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>City</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
                 <Dropdown>
                   <Dropdown.Toggle>Select the City</Dropdown.Toggle>
                 </Dropdown>
               </Form.Group>
             </Col>
 
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>State</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
                 <Dropdown>
                   <Dropdown.Toggle>Select the State</Dropdown.Toggle>
                 </Dropdown>
@@ -109,41 +140,57 @@ function DetailsPage() {
             </Col>
           </Row>
 
-          <Row className="mb-3">
+          <Row className="mb-4">
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Country</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridCountry">
-                <Form.Label>Country</Form.Label>
                 <Dropdown>
                   <Dropdown.Toggle>Select the Country</Dropdown.Toggle>
                 </Dropdown>
               </Form.Group>
             </Col>
 
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Zip Code</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip Code</Form.Label>
                 <Form.Control type="number" placeholder="Enter your Zip code" />
               </Form.Group>
             </Col>
           </Row>
 
-          <Row className="mb-3">
+          <Row className="mb-4">
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Father Name</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridFatherName">
-                <Form.Label>Father Name</Form.Label>
                 <Form.Control placeholder="Enter your father name" />
               </Form.Group>
             </Col>
 
+            <Col sm={2}>
+              <Form.Group as={Col}>
+                <Form.Label>Mother Name</Form.Label>
+              </Form.Group>
+            </Col>
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridMotherName">
-                <Form.Label>Mother Name</Form.Label>
                 <Form.Control placeholder="Enter your mother name" />
               </Form.Group>
             </Col>
           </Row>
 
-          <Row className="mb-3">
+          <Row className="mb-4">
             <Col sm={3}>
               <Form.Group as={Col} controlId="formGridSubmit">
                 <Button variant="primary" type="submit">
